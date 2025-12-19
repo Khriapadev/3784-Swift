51d1a328ae7bf50cdc8a67936506a6d132513d39
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-19 19:27:07 +0200

    init

---
+++ FruitFury/App/FruitFuryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitFuryApp.swift
+//  FruitFury
+//
+//  Created by Serhii Babchuk on 19.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct FruitFuryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FFLaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-FruitFury/Pods-FruitFury-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FruitFury : NSObject
+@end
+@implementation PodsDummy_Pods_FruitFury
+@end
